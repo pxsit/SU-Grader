ab7bf55092a8e7874a9a558991576c535efd37e0
Pasit Sangprachathanarak <ouipingpasit@gmail.com>
2026-08-31 21:54:36 +0800

    Update 3numsort.cpp

--- Grader/Basic If else/3numsort.cpp
+++ Grader/Basic If else/3numsort.cpp
@@ -1,14 +1,13 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::vector<int> a(3);
+    vector<int> a(3);
     for (int i = 0; i < 3; i++) {
-        std::cin >> a[i];
+        cin >> a[i];
     }
-    std::sort(a.begin(), a.end());
+    sort(a.begin(), a.end());
     for (int i = 0; i < 3; i++) {
-        std::cout << a[i] << ' ';
+        cout << a[i] << ' ';
     }
 }
